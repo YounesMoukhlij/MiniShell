32a581b8cbb056ead51403cbf32d4f3359d000cb
Younes Moukhlij <youmoukh@e3r7p11.1337.ma>
2024-04-13 14:43:57 +0000

    asd

--- Exec/signals/signal_handler_1.c
+++ Exec/signals/signal_handler_1.c
@@ -5,8 +5,9 @@ void	signal_handler_one(int sig_v)
 {
 	if (sig_v == SIGINT)
 	{
-		write(STDOUT_FILENO, "\n", 1);
-	
+		write(1, "\n", 1);
+		write(1, "nasdasdasd", 100);
+
 		// signal(SIGINT, signal_handler_one);
 	}
 }
