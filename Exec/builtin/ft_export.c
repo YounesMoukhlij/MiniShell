cdbe658950ab809087321a2829fdc004f1aed293
Younes Moukhlij <youmoukh@e3r3p2.1337.ma>
2024-05-05 18:33:21 +0100

    new

--- Exec/builtin/ft_export.c
+++ Exec/builtin/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 16:50:34 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/05/05 18:13:47 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/05/05 18:32:24 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ int	ft_export(t_minishell *mini, t_env *envir, int i)
 		while (mini->cmd[i])
 		{
 			if (is_exportable(mini, mini->cmd[i], envir)
-				&& err_export(mini->cmd[i]) && 
+				 && err_export(mini->cmd[i]) && 
 				!already_exist(mini->cmd[i], envir) && ft_strlen(mini->cmd[i]))
 			{
 				if (check_special_case(mini->cmd[i]))
