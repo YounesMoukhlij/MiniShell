fde646771a68093816a6113167f9f017f6693ca7
YounesMoukhlij <123767564+YounesMoukhlij@users.noreply.github.com>
2024-05-18 15:07:37 +0100

    new

--- Exec/builtin/ft_export.c
+++ Exec/builtin/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 16:50:34 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/05/16 21:23:56 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/05/18 15:07:07 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,11 +59,33 @@ int	c_d(t_env *env, char *s)
 	return (0x1);
 }
 
+t_env	*huge_export(t_minishell *m, int flag, t_env *env, int i)
+{
+	char	**p;
+	t_env	*lst;
+
+	lst = 0x0;
+	if (check_special_case(m->cmd[i]))
+		lst = lstnew_executor(ft_key(m->cmd[i]),
+				special_case(m->cmd[i], env), 0);
+	else if (no_equal(m->cmd[i]))
+		lst = lstnew_executor(ft_strdup_1(m->cmd[i]), NULL, 0);
+	else if (if_equal(m->cmd[i]))
+	{
+		p = ft_split_export(m->cmd[i]);
+		check_export(m, p[0x0]);
+		if (p[0x1][0] == SGL && p[0x1][ft_strlen(p[0x1]) - 1] == SGL)
+			flag = 1;
+		lst = lstnew_executor(p[0x0], p[0x1], flag);
+	}
+	else if (no_value(m->cmd[i]))
+		lst = lstnew_executor(ft_strdup_1(m->cmd[i]), ft_strdup_1(""), 0);
+	return (lst);
+}
+
 int	ft_export(t_minishell *mini, t_env *env, int i, int flag)
 {
 	t_env	**head;
-	t_env	*lst;
-	char	**p;
 
 	if (!mini->cmd[0x0])
 		return (0x0);
@@ -77,25 +99,7 @@ int	ft_export(t_minishell *mini, t_env *env, int i, int flag)
 			if (err_export(mini->cmd[i]))
 			{
 				if (already_exist(mini->cmd[i], env, 0x0, 0x0) && c_d(env, mini->cmd[i]))
-				{
-					if (check_special_case(mini->cmd[i]))
-						lst = lstnew_executor(ft_key(mini->cmd[i]),
-								special_case(mini->cmd[i], env), 0);
-					else if (no_equal(mini->cmd[i]))
-						lst = lstnew_executor(ft_strdup_1(mini->cmd[i]), NULL, 0);
-					else if (if_equal(mini->cmd[i]))
-					{
-						p = ft_split_export(mini->cmd[i]);
-						check_export(mini, p[0x0]);
-						if (p[0x1][0] == SGL && p[0x1][ft_strlen(p[0x1]) - 1] == SGL)
-							flag = 1;
-						lst = lstnew_executor(p[0x0], p[0x1], flag);
-					}
-					else if (no_value(mini->cmd[i]))
-						lst = lstnew_executor(ft_strdup_1(mini->cmd[i]), ft_strdup_1(""), 0);
-					add_back_executor(head, lst);
-					ex_st_f(0x0, 0x1);
-				}
+						add_back_executor(head, huge_export(mini, flag, env, i));
 			}
 			i++;
 		}
